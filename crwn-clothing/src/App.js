fbc3b8ac7f887dabb8c7753da5b8dc013aca045e
chenhunluo321 <32112516+chenhunluo321@users.noreply.github.com>
2020-05-17 16:06:59 -0700

    update with withRouter

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
-    <HomePage/>
+    <div>
+    <Switch>
+    <Route exact path = '/' component = {HomePage} />
+    <Route exact path = '/hats' component = {HatsPage} />
+    </Switch>
+    </div>
   );
 }
 
